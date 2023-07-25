192707c0a911a1073baea1a20af78a421caac98d
sinisanezic <128232162+sinisanezic@users.noreply.github.com>
2023-07-25 13:56:59 -0700

    Update SQLQuery_Total number of rides for whole year given in percentages for both rider types.sql

--- SQLQuery_Total number of rides for whole year given in percentages for both rider types.sql
+++ SQLQuery_Total number of rides for whole year given in percentages for both rider types.sql
@@ -8,4 +8,5 @@ SELECT rider_type,
 
 
 Note: We could complete this query without using CAST function. However, the result was given with extra zeroes in percentage column.
-      To remove extra zeroes, we use CAST function to cast the result of the ROUND function to DECIMAL (10, 2). The DECIMAL (10, 2) means that there will be a maximum of 10 digits with 2 of them representing decimal places
+      To remove extra zeroes, we use CAST function to cast the result of the ROUND function to DECIMAL (10, 2). 
+      The DECIMAL (10, 2) means that there will be a maximum of 10 digits with 2 of them representing decimal places.
