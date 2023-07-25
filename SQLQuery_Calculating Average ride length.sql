2694fc7674b4ba7099c29ff9aa537f4baec14d75
sinisanezic <128232162+sinisanezic@users.noreply.github.com>
2023-07-25 14:02:12 -0700

    Update SQLQuery_Calculating Average ride length.sql

--- SQLQuery_Calculating Average ride length.sql
+++ SQLQuery_Calculating Average ride length.sql
@@ -9,3 +9,7 @@ WHERE
     rider_type IN ('Casual', 'Member')
 GROUP BY
     rider_type
+
+Note: In this query the "DATEDIFF" function is used to calculate the difference between started_at_new and ended_at_new timestamps in minutes.
+    The AVG function is then applied to calculate the average duration for each distinct rider type.
+    The result set will include two raws , one for each rider type, along with their corresponding average ride duartions in minutes.
