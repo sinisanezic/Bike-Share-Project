55beb05f070d88343efe650770f4020d300b320c
sinisanezic <128232162+sinisanezic@users.noreply.github.com>
2023-07-25 15:15:00 -0700

    Update Total number of rides by day of week for both types of riders.sql

--- Total number of rides by day of week for both types of riders.sql
+++ Total number of rides by day of week for both types of riders.sql
@@ -14,3 +14,8 @@ SELECT rider_type,
 FROM Bike_Share_Project.dbo.full_year
 GROUP BY DATEPART(WEEKDAY, started_at_new), rider_type
 ORDER BY DATEPART(WEEKDAY, started_at_new)
+
+Note: The CASE statement is used to transform the numeric value of the day of the week(1 for Sunday, 7 for Saturday) into 
+a corresponding string representation(SUnday, Monday,etc). The DATEPART (WEEKDAY, started_at _new) function returns the numeric representation 
+of the day of the week for the started_at_new column, which likely contains the date and time when the bike ride started. The result 
+of this CASE statement will be an additional column named "day_of_week".
