0a09cab5b0fae748960f96026ae85fe67fae6550
sinisanezic <128232162+sinisanezic@users.noreply.github.com>
2023-07-25 15:18:15 -0700

    Update SQLQuery_Total number of rides each month for both types of riders.sql

--- SQLQuery_Total number of rides each month for both types of riders.sql
+++ SQLQuery_Total number of rides each month for both types of riders.sql
@@ -7,3 +7,6 @@ SELECT
 FROM Bike_Share_Project.dbo.full_year
 GROUP BY DATENAME(MONTH, started_at_new), rider_type
 ORDER BY MONTH
+
+Note: In this query, we are using the DATENAME function with the MONTH parameter to get the month names from the started_at_new column.
+This function will return the month name corresponding to the date in the started_at_new column. 
